a2170374a752fcd5dd7cadf7377f31a370a17102
Jyot Sarup <52942229+jyotSarup@users.noreply.github.com>
2020-08-30 16:49:46 -0700

    set up relations between mongoose and graphql

---
+++ app.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
+const app = express();
+
+// const events = [];
+
+app.use(bodyParser.json())
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String! 
+        }
+
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input UserInput {
+            email: String!
+            password: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event   
+            createUser(userInput: UserInput) : User
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+            .then(events => {
+                return events.map(event => {
+                    return {
+                        ...event._doc
+                    }
+                })
+            })
+            .catch(err=> console.log(err))
+        },
+        createEvent: args => {
+            // const event = {
+            //     _id : Math.random().toString(),
+            //     title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //     price: +args.eventInput.price,
+            //     date: args.eventInput.date
+            // }
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+                creator: '5f4b32da5f51163324559761'
+            });
+            // events.push(event);
+            let createdEvent;
+             return event
+             .save()
+             .then((result)=>{
+                createdEvent = result;
+                return User.findById('5f4b32da5f51163324559761');
+                // return result
+            })
+            .then(user => {
+                if(!user){
+                    throw new Error('User does not exist')
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            })
+            .then(result => {
+                return createdEvent;
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
+            return event 
+        },
+        createUser: args => {
+
+            return User.findOne({email: args.userInput.email}).then(user => {
+                if ( user ){
+                    throw new Error('User exists already.')
+                }
+
+                return bcrypt.hash(args.userInput.password, 12)
+            })
+            .then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword 
+                });
+                return user.save();
+            })
+            .then(result => {
+            //    return {...result};
+           
+            
+            return {...result._doc, password:null};
+            })
+            .catch(err => {
+                throw err;
+            })
+        }
+    },
+    graphiql: true
+}))
+
+app.get('/', (req,res,next) => {
+    res.send('Hey');
+})
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@testcluster.1tcwp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,{useUnifiedTopology: true,useNewUrlParser: true})
+.then(()=> {
+    app.listen(3000, console.log(`app listening at port 3000`)); 
+}).catch(err => {
+    console.log(err);
+});
+
